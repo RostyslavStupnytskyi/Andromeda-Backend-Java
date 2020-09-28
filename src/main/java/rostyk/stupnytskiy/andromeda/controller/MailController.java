0829f1bed24250adcdc06b6ebb970ca19b39920e
RostykStup <rostyk.stup@gmail.com>
2020-09-28 22:02:43 +0300

    changed email configuration, created working service

--- src/main/java/rostyk/stupnytskiy/andromeda/controller/MailController.java
+++ src/main/java/rostyk/stupnytskiy/andromeda/controller/MailController.java
@@ -1,8 +1,6 @@
 package rostyk.stupnytskiy.andromeda.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import rostyk.stupnytskiy.andromeda.dto.request.mail.MailRequest;
--- src/main/java/rostyk/stupnytskiy/andromeda/mail/MailService.java
+++ src/main/java/rostyk/stupnytskiy/andromeda/mail/MailService.java
@@ -1,10 +1,14 @@
 package rostyk.stupnytskiy.andromeda.mail;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import rostyk.stupnytskiy.andromeda.dto.request.mail.MailRequest;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.Properties;
 
 @Service
 public class MailService {
@@ -12,18 +16,14 @@ public class MailService {
     @Autowired
     private MailDataRepository mailDataRepository;
 
-    @Autowired
-    public JavaMailSender emailSender;
+    private Session session;
 
-    public void testSend() {
-        SimpleMailMessage message = new SimpleMailMessage();
-
-        message.setTo("rostyk.stup@gmail.com");
-        message.setSubject("Test Simple Email");
-        message.setText("Hello, Im testing Simple Email");
-
-        emailSender.send(message);
+    public MailService(){
+        MailData mailData = getMainMail();
+        session = mailConfig(mailData.getEmail(),mailData.getPassword());
     }
+
+//    sendEmail(session, toEmail, "TLSEmail Testing Subject", "TLSEmail Testing Body");
 
     public void saveMain(MailRequest request) {
         MailData mailData = mailRequestToMailData(request);
@@ -46,5 +46,45 @@ public class MailService {
         mailData.setEmail(request.getLogin());
         mailData.setPassword(request.getPassword());
         return mailData;
+    }
+
+    private Session mailConfig(String email, String password) {
+        System.out.println("TLSEmail Start");
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
+        props.put("mail.smtp.port", "587"); //TLS Port
+        props.put("mail.smtp.auth", "true"); //enable authentication
+        props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
+
+        //create Authenticator object to pass in Session.getInstance argument
+        Authenticator auth = new Authenticator() {
+            @Override //override the getPasswordAuthentication method
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(email, password);
+            }
+        };
+        Session session = Session.getInstance(props, auth);
+        return session;
+    }
+
+    private void sendEmail(String target, String subject, String body) {
+        try {
+            MimeMessage msg = new MimeMessage(session);
+            //set message headers
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+            msg.addHeader("format", "flowed");
+            msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+            msg.setFrom(new InternetAddress("no_reply@example.com", "NoReply-JD"));
+            msg.setReplyTo(InternetAddress.parse("no_reply@example.com", false));
+            msg.setSubject(subject, "UTF-8");
+            msg.setText(body, "UTF-8");
+            msg.setSentDate(new Date());
+
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(target, false));
+            Transport.send(msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
