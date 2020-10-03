d6a093d6731788c7496ce8127ba11fe4911a86d7
RostykStup <rostyk.stup@gmail.com>
2020-10-03 15:48:53 +0300

    Changed resourses url

--- src/main/java/rostyk/stupnytskiy/andromeda/config/StaticResourcesConfiguration.java
+++ src/main/java/rostyk/stupnytskiy/andromeda/config/StaticResourcesConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.File;
 import java.nio.file.Paths;
 
 import static rostyk.stupnytskiy.andromeda.tools.FileTool.PROJECT_DIR;
@@ -13,7 +14,7 @@ public class StaticResourcesConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**/**")
+        registry.addResourceHandler("/image/**/**/**")
                 .addResourceLocations(Paths.get(PROJECT_DIR).toUri().toString());
     }
 }
