911cfbb7c82eb158785b66724b3fbc2259c15201
RostykStup <rostyk.stup@gmail.com>
2020-10-12 22:54:20 +0300

    order repository

---
+++ src/main/java/rostyk/stupnytskiy/andromeda/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package rostyk.stupnytskiy.andromeda.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import rostyk.stupnytskiy.andromeda.entity.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long> {
+}
