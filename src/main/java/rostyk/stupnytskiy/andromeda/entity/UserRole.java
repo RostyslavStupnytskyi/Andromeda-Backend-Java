b5f4750c0982b6480b81a74e9b05192d06eeea41
RostykStup <rostyk.stup@gmail.com>
2020-10-26 00:01:35 +0200

    Updated sequrity config

--- src/main/java/rostyk/stupnytskiy/andromeda/entity/UserRole.java
+++ src/main/java/rostyk/stupnytskiy/andromeda/entity/UserRole.java
@@ -1,5 +1,5 @@
 package rostyk.stupnytskiy.andromeda.entity;
 
 public enum UserRole {
-    ROLE_USER, ROLE_MAIN_ADMIN,ROLE_ADMIN,ROLE_MODERATOR,ROLE_SELLER
+    ROLE_USER, ROLE_MAINADMIN,ROLE_ADMIN,ROLE_MODERATOR,ROLE_SELLER
 }
